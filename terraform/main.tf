b12574d8bc21078b1adef654ac12288368216a7a
Anila Gokada <anila.gokada.87@gmail.com>
2026-02-04 14:27:03 +0530

    Add Terraform configuration for Google Cloud Run

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_cloud_run_service" "app" {
+  name     = "demo-app"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.image_uri
+
+        ports {
+          container_port = 8080
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "public" {
+  service  = google_cloud_run_service.app.name
+  location = var.region
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+output "url" {
+  value = google_cloud_run_service.app.status[0].url
+}
