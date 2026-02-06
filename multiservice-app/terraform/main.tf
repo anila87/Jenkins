b0e83422f67a2d34e921c499861e5ade22ceac25
Anila Gokada <anila.gokada.87@gmail.com>
2026-02-06 15:31:41 +0530

    Update main.tf

--- multiservice-app/terraform/main.tf
+++ multiservice-app/terraform/main.tf
@@ -49,17 +49,17 @@ resource "google_cloudfunctions_function" "service_b" {
 }
 
 
-// resource "google_storage_bucket" "functions_bucket" {
-//   name          = var.bucket
-//   location      = var.region
-//   force_destroy = true
+ resource "google_storage_bucket" "functions_bucket" {
+   name          = var.bucket
+   location      = var.region
+   force_destroy = true
 
-//   uniform_bucket_level_access = true
-// }
+   uniform_bucket_level_access = true
+ }
 
 
-// resource "google_storage_bucket_object" "functions_archive" {
-//   name   = "service-b.zip"
-//   bucket = google_storage_bucket.functions_bucket.name
-//   source = "../service-b/service-b.zip"
-// }
+ resource "google_storage_bucket_object" "functions_archive" {
+   name   = "service-b.zip"
+   bucket = google_storage_bucket.functions_bucket.name
+   source = "../service-b/service-b.zip"
+ }
